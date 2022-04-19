72485e89737c544f425928aa3a94141658bf4adc
Mitchell Otterstrom <ottermit@gmail.com>
2022-04-18 19:32:56 -0500

    Successfully can create any size grid.

--- js-file.js
+++ js-file.js
@@ -1,19 +1,33 @@
 const container = document.querySelector('.container');
+const clearBtn = document.querySelector('#clearBtn');
 
 function createBlock() {
     const square = document.createElement('div');
-    //square.style.height = '10px';
-    //square.style.width = '10px';
     square.style.border = '1px solid black';
+    square.addEventListener("mouseenter", function(event) {
+        event.target.style.backgroundColor = 'red';
+    });
+    /*square.addEventListener("mouseleave", function(event) {
+        event.target.style.backgroundColor = 'white';
+    }); */
     container.appendChild(square);
 }
 
-function createGrid () {
-   let numRows = 16;
+let gridSize = 16;
 
-   for (let i = 0; i < numRows * numRows; i++) {
-       createBlock();
-   }
+function createGrid() {
+    gridSize = prompt('What size grid would you like?');
+    let gridTemplateColumns = 'repeat('+gridSize+', 1fr)';
+    container.style.gridTemplateColumns = gridTemplateColumns;
+    container.style.gridTemplateRows = gridTemplateColumns;
+
+    for (let i = 0; i < gridSize * gridSize; i++) {
+        createBlock();
+    }
+}
+
+function resetGrid() {
+
 }
 
 createGrid();
